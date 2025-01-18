cefdf1539a9e617ecdcf2270d22609dd3f60f576
mornepic <thomas.chaufoureau@univ-tlse3.fr>
2025-01-18 18:15:21 +0100

    a

--- my_pages/page_predictions.py
+++ my_pages/page_predictions.py
@@ -11,79 +11,46 @@ import matplotlib.pyplot as plt
 
 def create_pred(pd_hist) :
 
-    # Création des dates au format datetime (pour un plot plus clair)
-    # Convertir la colonne "Annee" au format datetime
-    pd_hist["Annee"] = pd.to_datetime(pd_hist["Annee"], format='%Y', errors='coerce')
-
-    # Extraire l'année (inutile dans ce cas, mais inclus pour cohérence avec votre code)
-    pd_hist["Annee"] = pd_hist["Annee"].dt.year
-
-    
-
     # Affichage graphique du résultat
-    #split_index = pd_hist[pd_hist['Annee'] >= 2025].index[0]
-
-    noir = pd_hist[pd_hist['Annee'] <= 2025]
-    bleu = pd_hist[pd_hist['Annee'] >= 2025]
+    split_index = pd_hist[pd_hist['Annee'] >= 2025].index[0]
 
     #   Tracer la courbe continue avec deux segments de couleur
     fig, ax = plt.subplots(figsize=(10, 6))
 
     # Première partie : avant 2025 (en noir)
     ax.plot(
-        noir['Annee'],
-        noir['Total'],
+        pd_hist['Annee'][:split_index + 1],
+        pd_hist['Total'][:split_index + 1],
         color='black', label='Avant 2025'
     )
 
     # Deuxième partie : 2025 et après (en bleu)
     ax.plot(
-        bleu['Annee'],
-        bleu['Total'],
+        pd_hist['Annee'][split_index:],
+        pd_hist['Total'][split_index:],
         color='blue', label=' Prédiction après 2025'
     )
 
     # Troisième partie : avant 2025 (en noir)
     ax.plot(
-        noir['Annee'],
-        noir['Total_max'],
+        pd_hist['Annee'][:split_index + 1],
+        pd_hist['Total_max'][:split_index + 1],
         color='white'
     )
 
     # Quatrième partie : 2025 et après (en bleu)
     ax.plot(
-        bleu['Annee'],
-        bleu['Total_max'],
+        pd_hist['Annee'][split_index:],
+        pd_hist['Total_max'][split_index:],
         color='white'
     )
 
     # Cinquième partie : fixation des objectifs
-    ax.plot(pd_hist['Annee'], [400000] * 20, color='red', linestyle='--')
-    ax.scatter(2030, 400000, color='red', label='Objectif Horizon 2030', zorder=5)
-
-    ax.set_xticks(pd_hist["Annee"])
+    ax.plot(pd_hist['Annee'], [400000] * 20, color='red', linestyle='--', label='Objectif Horizon 2030')
 
     # Intervalles de confiance
-    #ax.fill_between(pd_hist['Annee'][:split_index + 1], pd_hist['Total'][:split_index + 1], pd_hist['Total_max'][:split_index + 1], color="gray", alpha=0.3) # Pour les réels
-    #ax.fill_between(pd_hist['Annee'][split_index:], pd_hist['Total'][split_index:], pd_hist['Total_max'][split_index:], color="gray", alpha=0.3, label=" Marge de 30% de bornes supplémentaires") # Pour les prédictions
-
-    ax.fill_between(
-        noir['Annee'],
-        noir['Total'],
-        noir['Total_max'],
-        color="gray",
-        alpha=0.3
-    )
-
-    # Pour la partie à partir de 2025
-    ax.fill_between(
-        bleu['Annee'],
-        bleu['Total'],
-        bleu['Total_max'],
-        color="gray",
-        alpha=0.3,
-        label="Marge de 30% de bornes supplémentaires"
-    )
+    ax.fill_between(pd_hist['Annee'][:split_index + 1], pd_hist['Total'][:split_index + 1], pd_hist['Total_max'][:split_index + 1], color="gray", alpha=0.3) # Pour les réels
+    ax.fill_between(pd_hist['Annee'][split_index:], pd_hist['Total'][split_index:], pd_hist['Total_max'][split_index:], color="gray", alpha=0.3, label="Intervalle de Confiance 30 %") # Pour les prédictions
 
     # Ajouter des légendes et des étiquettes
     ax.set_xlabel("Année")
@@ -102,22 +69,109 @@ def create_pred(pd_hist) :
 
 
 
-def show(bornes_pred,pred_reg, pred_ve, dico_graphes):
-    # st.title("Page 3 : Page de prédiction")
+def show(bornes_pred,pred_reg, pred_ve):
+    st.title("Page 3 : Page de prédiction")
+    st.write("Bienvenue sur la page de prédiction.")
+    st.write("Prédiction de l'évolution du nombre de bornes")
     create_pred(bornes_pred)
     # Ensuite on affiche le graphique de prédiction globale sur les véhicules (Raissa)
-    st.markdown("---")
-    col1, col2 = st.columns([1, 1])
-    # Liste des trimestres (les clés du dictionnaire)
-    with col2:
-        region = st.selectbox(
-        "Selection Région :",
-        options=['Total','Île-de-France','Centre-Val de Loire','Bourgogne-Franche-Comté','Normandie',
- 'Hauts-de-France','Grand Est','Pays de la Loire','Bretagne','Nouvelle-Aquitaine','Occitanie',
- 'Auvergne-Rhône-Alpes',"Provence-Alpes-Côte d'Azur",'Corse']
-        )
-    code = f"forecast_{region}.html"
-    with col1:
-        st.title("Prédiction du nombre de véhicules électriques")
-    graphe_html_choisi = dico_graphes[code]
-    st.components.v1.html(graphe_html_choisi, height=1050, width=1350)
+    st.write("Prédiction de l'évolution du nombre de véhicules électriques")
+
+    pred_ve["date_arrete"] = pd.to_datetime(pred_ve["date_arrete"])
+    pred_ve["Annee"] = pred_ve["date_arrete"].dt.year
+    
+
+
+    noir = pred_ve[pred_ve['Annee'] <= 2025]
+    bleu = pred_ve[pred_ve['Annee'] >= 2025]
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+
+    ax.plot(
+        noir['date_arrete'],
+        noir['prediction_nb_vp_rechargeables_el'],
+        color='black', label='Avant 2025'
+    )
+
+
+    ax.plot(
+        bleu['date_arrete'],
+        bleu['prediction_nb_vp_rechargeables_el'],  
+        color='blue', label=' Prédiction après 2025'       
+    )
+
+    # Cinquième partie : fixation des objectifs
+    ax.plot(pred_ve['date_arrete'], [4800000] * len(pred_ve['Annee']), color='red', linestyle='--', label='Objectif Horizon 2030')
+
+    ax.set_xlabel("Année")
+    ax.set_ylabel("Valeur")
+    ax.set_title("Évolution des nombres de véhicules électriques")
+    ax.legend()
+    ax.grid(True)
+
+    # Afficher le graphique dans Streamlit
+    st.pyplot(fig)
+
+
+
+
+
+    # On s'occupe de la prédiction du ratio par régions avec le ratio de 1 borne 10 véhicules
+
+    st.write("Prédictions par régions")
+    st.sidebar.header("Filtres")
+    options = ["Toutes les régions"] + \
+            sorted(pred_reg['Nom_Région'].unique()) 
+    selected_option = st.sidebar.selectbox(
+            "Sélectionnez une région", options=options)
+    
+    
+    pred_reg = pred_reg[pred_reg['Nom_Région'] == selected_option] 
+
+    # On tri les valeurs par années de façon ascendante
+    pred_reg = pred_reg.sort_values(by='date_arrete')
+
+    # Déterminer le suffixe du titre dynamique
+    title_suffix = ""
+    if selected_option  != "Toutes les régions" : 
+        title_suffix = f" - {selected_option}"
+    
+    # Titre graph
+    st.subheader(f"Analyse du nombre de véhicules électriques{title_suffix}")
+
+    pred_reg["date_arrete"] = pd.to_datetime(pred_reg["date_arrete"])
+    pred_reg["Annee"] = pred_reg["date_arrete"].dt.year
+    #split_index = pred_reg[pred_reg['Annee'] >= 2025].index[0] 
+
+
+    noir = pred_reg[pred_reg['Annee'] <= 2025]
+    bleu = pred_reg[pred_reg['Annee'] >= 2025]
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+    print(pred_reg)
+
+    ax.plot(
+        noir['date_arrete'],
+        noir['nb_vp_rechargeables_el'],
+        color='black', label='Avant 2025'
+    )
+
+    # pred_reg['date_arrete'][:split_index + 1]
+    # pred_reg['nb_vp_rechargeables_el'][:split_index + 1]
+    # Deuxième partie : 2025 et après (en bleu)
+    ax.plot(
+        bleu['date_arrete'],
+        bleu['nb_vp_rechargeables_el'],  # Problème : il n'y a que la partie bleu qui s'affiche (elle ne s'arrète pas)
+        color='blue', label=' Prédiction après 2025'       # Voir sur le split index...
+    )
+
+    #pred_reg['date_arrete'][split_index:],
+    #pred_reg['nb_vp_rechargeables_el'][split_index:]
+    ax.set_xlabel("Année")
+    ax.set_ylabel("Valeur")
+    ax.set_title("Évolution des nombres de véhicules électriques")
+    ax.legend()
+    ax.grid(True)
+
+    # Afficher le graphique dans Streamlit
+    st.pyplot(fig)
